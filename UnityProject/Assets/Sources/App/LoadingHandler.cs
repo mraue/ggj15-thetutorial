e701806b92c93261016e0b9708011b74ed785f55
Martin Raue <martin.raue@gmail.com>
2015-01-24 09:46:08 +0100

    Moar directories ftw

---
+++ UnityProject/Assets/Sources/App/LoadingHandler.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace GGJ15.TheTutorial
+{
+	public class LoadingHandler : MonoBehaviour
+	{
+		public const string MAIN_SCENE = "Main";
+
+		void Start()
+		{
+			Application.LoadLevel(MAIN_SCENE);
+		}
+	}
+}
