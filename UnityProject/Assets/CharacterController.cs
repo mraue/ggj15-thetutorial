e5ca8b5a9409e7f07a6e5b09d5508bcd329f3b94
Nickolas Marouchos <nickolas.marouchos@wooga.net>
2015-01-24 11:15:49 +0100

    First pass on character controller and game colliders

---
+++ UnityProject/Assets/CharacterController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterController : MonoBehaviour {
+
+	public float speed = 5f;
+	bool directionRight = true;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		float move = Input.GetAxis ("Horizontal");
+
+		rigidbody2D.velocity = new Vector2(move * speed, rigidbody2D.velocity.y);
+
+
+	}
+}
