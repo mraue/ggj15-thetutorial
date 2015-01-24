663728523d8fdeb4fb064af0b1f384222743eaa2
Nickolas Marouchos <nickolas.marouchos@wooga.net>
2015-01-24 12:52:36 +0100

    Renamed Character Controller to player controller

--- UnityProject/Assets/Sources/Services/PlayerController.cs
+++ UnityProject/Assets/Sources/Services/PlayerController.cs
@@ -1,23 +1,35 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class CharacterController : MonoBehaviour {
+public class PlayerController : MonoBehaviour {
 
 	public float speed = 5f;
 	bool directionRight = true;
 
+	bool atExit = false;
+	
 	// Use this for initialization
 	void Start () {
-	
 	}
 	
 	// Update is called once per frame
-	void Update () {
-	
+	void FixedUpdate () {
 		float move = Input.GetAxis ("Horizontal");
-
 		rigidbody2D.velocity = new Vector2(move * speed, rigidbody2D.velocity.y);
-
-
 	}
+	
+	void OnTriggerEnter2D (Collider2D other) {
+		Debug.Log ("hello");
+		atExit = true;
+	}
+	
+	void OnTriggerExit2D (Collider2D other) {
+		Debug.Log ("goodbye");
+		atExit = false;
+	}
+
+	bool returnExit (){
+		return atExit;
+	}
+
 }
