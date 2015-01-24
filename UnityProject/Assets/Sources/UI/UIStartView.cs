c935daf23e95611e3ba140469a0b121ede3f1f07
Nickolas Marouchos <nickolas.marouchos@wooga.net>
2015-01-24 18:32:55 +0100

    Added animations for end of game screen

--- UnityProject/Assets/Sources/UI/UIStartView.cs
+++ UnityProject/Assets/Sources/UI/UIStartView.cs
@@ -1,19 +1,25 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using System.Collections.Generic;
 using DG.Tweening;
 
 namespace GGJ15.TheTutorial
 {
 	public class UIStartView : MonoBehaviour
 	{
-		//Animation _animation;
-		
+
 		public Image backgroundImage;
+
+		public List<AnimationClip> animationList;
+		int _animationIndex = 0;
+		Animation _animation;
+		int _levelMax;
 
 		void Awake()
 		{
-		//	_animation = GetComponent<Animation>();
+			_animation = GetComponent<Animation>();
+			StartAnimation(2);
 		}
 
 		public void OnStart()
@@ -21,7 +27,25 @@ namespace GGJ15.TheTutorial
 			Hide(false);
 			GameContext.currentInstance.director.OnUIStart();
 		}
-		
+
+		public void StartAnimation(int level)
+		{
+			Debug.Log("StartAnimation");
+			_animationIndex = 0;
+			_animation.Play (animationList[_animationIndex].name);
+			_levelMax = level;
+
+		}
+
+		void Update()
+		{
+			if(!_animation.isPlaying && _animationIndex < _levelMax){
+				_animationIndex++;
+				_animation.Play (animationList[_animationIndex].name);
+			}
+
+		}
+
 		public void Show()
 		{
 			//	_animation.CrossFade("Show");
